4452854bb112f6803bf1419c55d2ce813ecb99f7
keyiflerolsun <keyiflerolsun@users.noreply.github.com>
2025-12-03 18:47:13 +0000

    ♻️ Domain Değişikliği
    🔄 Otomatik domain güncellemeleri yapıldı.

--- DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
+++ DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
@@ -14,7 +14,7 @@ import okhttp3.Response
 import org.jsoup.Jsoup
 
 class Dizipal : MainAPI() {
-    override var mainUrl              = "https://dizipal1515.com/"
+    override var mainUrl              = "https://dizipal1515.com"
     override var name                 = "Dizipal"
     override val hasMainPage          = true
     override var lang                 = "tr"
